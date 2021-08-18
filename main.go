9ff4cf73ce4856744ee6475bfbacf2d0263f62c1
Mark Bradley <braddle@gmail.com>
2021-08-18 14:20:59 +0100

    Handling Running Script without required arguments
    Making the first end to end test pass

--- main.go
+++ main.go
@@ -1,4 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
+	fmt.Println("Error: script requires input and output file locations to be provided")
+	fmt.Println("\t ./sam <input file location> <output file location>")
+	os.Exit(1)
 }
