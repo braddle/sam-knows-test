8ce405aef3341b6e99bba7b046d2550e5b576da7
Mark Bradley <braddle@gmail.com>
2021-08-24 08:03:24 +0100

    Refactoring Measure Tests
    Removing duplication of test data.

--- data/measure_test.go
+++ data/measure_test.go
@@ -15,13 +15,16 @@ func TestMeasureSuite(t *testing.T) {
 	suite.Run(t, new(MeasureSuite))
 }
 
-func (s *MeasureSuite) TestGetStartDate() {
-	exp := time.Date(2020, 06, 01, 12, 0, 0, 0, time.UTC)
-	m := data.Measurements{
- 		M: []data.Measure{
- 			{
- 				Time:   exp,
- 				Metric: 10.5,
+var m data.Measurements
+var startDate = time.Date(2020, 06, 01, 12, 0, 0, 0, time.UTC)
+var endDate = time.Date(2020, 06, 04, 12, 0, 0, 0, time.UTC)
+
+func (s *MeasureSuite) SetupTest() {
+	m = data.Measurements{
+		M: []data.Measure{
+			{
+				Time:   startDate,
+				Metric: 10.5,
 			},
 			{
 				Time: time.Date(2020, 06, 02, 12, 0,0,0, time.UTC),
@@ -32,115 +35,30 @@ func (s *MeasureSuite) TestGetStartDate() {
 				Metric: 8.2,
 			},
 			{
-				Time: time.Date(2020, 06, 04, 12, 0,0,0, time.UTC),
+				Time: endDate,
 				Metric: 11.99,
 			},
 		},
- 	}
+	}
+}
 
- 	s.Equal(exp, m.GetStartDate())
+func (s *MeasureSuite) TestGetStartDate() {
+ 	s.Equal(startDate, m.GetStartDate())
 }
 
 func (s *MeasureSuite) TestGetEndDate() {
-	exp := time.Date(2020, 06, 04, 12, 0, 0, 0, time.UTC)
-	m := data.Measurements{
-		M: []data.Measure{
-			{
-				Time:   time.Date(2020, 06, 01, 12, 0, 0, 0, time.UTC),
-				Metric: 10.5,
-			},
-			{
-				Time: time.Date(2020, 06, 02, 12, 0,0,0, time.UTC),
-				Metric: 9.5,
-			},
-			{
-				Time: time.Date(2020, 06, 03, 12, 0,0,0, time.UTC),
-				Metric: 8.2,
-			},
-			{
-				Time:   exp,
-				Metric: 11.99,
-			},
-		},
-	}
-
-	s.Equal(exp, m.GetEndDate())
+	s.Equal(endDate, m.GetEndDate())
 }
 
 func (s *MeasureSuite) TestGetMinimumInMBPS() {
-	exp := 8.2
-	m := data.Measurements{
-		M: []data.Measure{
-			{
-				Time:   time.Date(2020, 06, 01, 12, 0, 0, 0, time.UTC),
-				Metric: 10.5,
-			},
-			{
-				Time: time.Date(2020, 06, 02, 12, 0,0,0, time.UTC),
-				Metric: 9.5,
-			},
-			{
-				Time:   time.Date(2020, 06, 03, 12, 0,0,0, time.UTC),
-				Metric: exp,
-			},
-			{
-				Time:   time.Date(2020, 06, 04, 12, 0, 0, 0, time.UTC),
-				Metric: 11.99,
-			},
-		},
-	}
-
-	s.Equal(exp, m.GetMinimumInMBPS())
+	s.Equal(8.2, m.GetMinimumInMBPS())
 }
 
 func (s *MeasureSuite) TestGetMaximumInMBPS() {
-	exp := 11.99
-	m := data.Measurements{
-		M: []data.Measure{
-			{
-				Time:   time.Date(2020, 06, 01, 12, 0, 0, 0, time.UTC),
-				Metric: 10.5,
-			},
-			{
-				Time: time.Date(2020, 06, 02, 12, 0,0,0, time.UTC),
-				Metric: 9.5,
-			},
-			{
-				Time:   time.Date(2020, 06, 03, 12, 0,0,0, time.UTC),
-				Metric: 8.2,
-			},
-			{
-				Time:   time.Date(2020, 06, 04, 12, 0, 0, 0, time.UTC),
-				Metric: exp,
-			},
-		},
-	}
-
-	s.Equal(exp, m.GetMaximumInMBPS())
+	s.Equal(11.99, m.GetMaximumInMBPS())
 }
 
 func (s *MeasureSuite) TestGetAverageInMBPS() {
-	m := data.Measurements{
-		M: []data.Measure{
-			{
-				Time:   time.Date(2020, 06, 01, 12, 0, 0, 0, time.UTC),
-				Metric: 10.5,
-			},
-			{
-				Time: time.Date(2020, 06, 02, 12, 0,0,0, time.UTC),
-				Metric: 9.5,
-			},
-			{
-				Time:   time.Date(2020, 06, 03, 12, 0,0,0, time.UTC),
-				Metric: 8.2,
-			},
-			{
-				Time:   time.Date(2020, 06, 04, 12, 0, 0, 0, time.UTC),
-				Metric: 11.99,
-			},
-		},
-	}
-
 	s.Equal(float64(10.0475), m.GetAverageInMBPS())
 }
 
@@ -174,26 +92,6 @@ func (s *MeasureSuite) TestGetMedianInMBPSOddNumberOfMetrics() {
 }
 
 func (s *MeasureSuite) TestGetMedianInMBPSEvenNumberOfMetrics() {
-	m := data.Measurements{
-		M: []data.Measure{
-			{
-				Time:   time.Date(2020, 06, 01, 12, 0, 0, 0, time.UTC),
-				Metric: 10.5,
-			},
-			{
-				Time: time.Date(2020, 06, 02, 12, 0,0,0, time.UTC),
-				Metric: 9.5,
-			},
-			{
-				Time:   time.Date(2020, 06, 03, 12, 0,0,0, time.UTC),
-				Metric: 8.2,
-			},
-			{
-				Time:   time.Date(2020, 06, 04, 12, 0, 0, 0, time.UTC),
-				Metric: 11.99,
-			},
-		},
-	}
 
 	s.Equal(float64(10), m.GetMedianInMBPS())
 }
